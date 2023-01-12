6f9061ea7ce7dd8b7326ad436e27e7a06749339c
Nikita Usov <nikitariusov@gmail.com>
2023-01-12 21:58:51 +0200

    add adaptive

--- lamborghini/js/main.js
+++ lamborghini/js/main.js
@@ -1,11 +1,28 @@
+const menuBtn = document.querySelector('.menu-btn')
+const menuMobile = document.querySelector('.menu-mobile')
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open')
+  menuBtn.classList.toggle('menu-btn--open')
+}, false)
+
 const swiper = new Swiper('.swiper', {
   slidesPerView: 2,
   spaceBetween: 20,
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-left',
-    prevEl: '.swiper-button-right',
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
   },
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+	},
 });
 
 let map;
@@ -141,5 +158,6 @@ function initMap() {
   ],
   });
 }
+
 
 window.initMap = initMap;
